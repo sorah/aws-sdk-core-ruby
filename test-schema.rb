d6875b16bcd7c5200c878391eb654830bf0e7427
Trevor Rowe <trevorrowe@gmail.com>
2014-04-22 10:57:11 -0700

    Updated SNS description.

--- test-schema.rb
+++ test-schema.rb
@@ -7,6 +7,7 @@ require 'json-schema'
   " S3" => 'apis/source/s3-2006-03-01.resources.json',
   "IAM" => 'apis/source/iam-2010-05-08.resources.json',
   "SQS" => 'apis/source/sqs-2012-11-05.resources.json',
+  "SNS" => 'apis/source/sns-2010-03-31.resources.json',
 }.each do |svc, path|
   json = JSON.load(File.read(path))
   errors = JSON::Validator.fully_validate('resources.schema.json', json)
