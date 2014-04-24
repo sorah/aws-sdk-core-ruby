0480bb2892d93a661effbafc9fa3b59acffdc5f7
Trevor Rowe <trevorrowe@gmail.com>
2014-04-24 13:20:25 -0700

    More work on ec2 model.

--- test-schema.rb
+++ test-schema.rb
@@ -5,6 +5,7 @@ require 'json-schema'
 
 {
   " S3" => 'apis/source/s3-2006-03-01.resources.json',
+  "EC2" => 'apis/source/ec2-2014-02-01.resources.json',
   "IAM" => 'apis/source/iam-2010-05-08.resources.json',
   "SQS" => 'apis/source/sqs-2012-11-05.resources.json',
   "SNS" => 'apis/source/sns-2010-03-31.resources.json',
